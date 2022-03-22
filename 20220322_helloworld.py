ddab0c03080df97375e5001b7b9af5f1b442817a
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-03-22 10:14:06 +0900

    변수 선언, 초기화, 여러 가지 출력하는 방법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,3 +1,8 @@
-print("Hello, World!") #큰따(""), 작따('') 같음
-print('Hello, JAYUN') #은 자바에서 //느낌(=주석)
+print("Hello, World!") #큰따(""), 작따('') 같음 #은 자바에서 //느낌(=주석)
 print("Red Velvet's favorite song is Red Flavor") #'을 쓰게 될때 큰따옴표를 해준다
+
+name='양다연'
+print('Hello, '+name+'!') #JAVA 형식
+print('Hello, %s!' % name) #C언어 형식
+print('Hello, {}!'.format(name)) #format 함수
+print(f'Hello, {name}!') # f-string 함수
