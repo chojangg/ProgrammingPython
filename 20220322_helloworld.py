423192d944f2add845028c1c841b50881d4f60fd
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-04-04 23:36:44 +0900

    변수, int, float, str, \n, type(), //:몫, %:나머지

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,8 +1,11 @@
 print("Hello, World!") #큰따(""), 작따('') 같음 #은 자바에서 //느낌(=주석)
-print("Red Velvet's favorite song is Red Flavor") #'을 쓰게 될때 큰따옴표를 해준다
-
 name='양다연'
 print('Hello, '+name+'!') #JAVA 형식
 print('Hello, %s!' % name) #C언어 형식
 print('Hello, {}!'.format(name)) #format 함수
-print(f'Hello, {name}!') # f-string 함수
+print(f'Hello, {name}!') # f-string 형식
+print("Red Velvet's favorite song is Red Flavor") #'을 쓰게 될때 큰따옴표를 해준다
+
+student_number = 2112
+name = '최자윤'
+print(f'학번: {student_number}, 이름: {name}')     #학번: 2112, 이름: 최자윤
---
+++ 20220329_type.py
@@ -0,0 +1,24 @@
+#숫자
+student_number = 2112
+age = 18
+height = 166.5
+
+#문자
+name = '최자윤'
+
+print(f'학번: {student_number}\n이름: {name}\n나이: {age}\n키: {height}')     #학번: 2112
+
+print(type(student_number))  #<class 'int'>
+print(type(name))  #<class 'str'>
+print(type(age))  #<class 'int'>
+print(type(height))  #<class 'float'>
+print(type(10.27))   #<class 'float'>
+print(type(1027))   #<class 'int'>
+print(type('1027'))   #<class 'int'>
+print(10/27)   #java: 0, python: 0.37037037037037035
+print(27/10)   #java: 2, python: 2.7
+print(27//10)   #python: 나눈 몫: 2
+print(27%10)   #python: 나머지: 7
+print(type(10/27))   #<class 'float'>
+print(type(10//27))   #<class 'int'>
+print(10/5) #2.0     <class 'float'>
