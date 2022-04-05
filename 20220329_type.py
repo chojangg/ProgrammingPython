0af317ed73140a5afb93aeadd818999cd571034f
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-04-05 09:50:55 +0900

    str(), int(), float(), +연산자, *연산자

--- 20220329_type.py
+++ 20220329_type.py
@@ -22,3 +22,22 @@ print(27%10)   #python: 나머지: 7
 print(type(10/27))   #<class 'float'>
 print(type(10//27))   #<class 'int'>
 print(10/5) #2.0     <class 'float'>
+
+#변수 이름 규칙, 자료형변환
+#my mbti, my_function(), MyClass  #myMbti: camel-case, my_mbti: snake-case
+my_mbti = 'INFP'
+print(f'MY MBTI: {my_mbti}')
+
+print('MY MBTI: '+my_mbti+', age: '+str(age))      #java: String.toString(age); python: str(age)
+height = '166.5'
+print(float(height) + 10)   #java: Float.parseFloat(height); python: float(height)
+
+#str(), int(), float(): 자료형변환 함수
+# +연산자: 숫자+숫자 => 덧셈, 문자+문자 => 문자문자
+# *연산자: 숫자*숫자 => 곱셈, 문자*숫자 => 문자를 숫자만큼 반복
+print(18 + 2)     #20
+print('18'+'12')    #'182'
+print(18 * 2)     #36
+print('2' * 4)     #'2222'
+#짝을 10번 출력
+print('짝' * 10)
