7f7d5034dd4105815fbc220f7615105dfee8620d
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-04-19 10:10:12 +0900

    str 연습. 문자열 함수(len, count, upper, lower, strip, replace, find, index), iin, split, join

--- 20220419_string.py
+++ 20220419_string.py
@@ -36,3 +36,39 @@ print(f'{student_number[0:2]}학년반')
 print(f'{student_number[0:-2]}학년반')
 print(f'{student_number[:-2]}학년반')  #start:end-1    [:end-1]: 앞까지
 print(f'{student_number[:]}')  #start:end-1    [:]: 앞~끝까지
+
+#문자열 함수
+print(f'길이: {len(student_number)}') #4
+print(f'2개수: {student_number.count("2")}')  #2 .count가 문자에서 특정문자가 몇개있는지 알려줌
+print(f'{"NCT dream darling".upper()}')
+print(f'{"NCT dream darling".lower()}')
+s="   NCT dream buffering      "
+print(f'{s.strip()}')   #strip=벗기다 띄워쓰기가 없어짐
+print(f'{s.lstrip()}')  #ls 왼쪽 띄워쓰기가 없어짐
+print(f'{s.rstrip()}')  #rs 오른쪽 띄워쓰기가 없어짐
+print(f'{s.find("e")}') #[7]
+print(f'{s.find("z")}') #없으면 -1
+print(f'{s.rfind("e")}') #17
+print(f'{s.count("e")}') #2
+print(f'{s.index("d")}') #7
+# print(f'{s.index("z")}') #없으면 ValueError: substring not found
+print(f'{s.replace("buffering", "hello future")}')  #replace하면 원본이 바뀌는게 아니라
+print(s)                                            #잠시 바뀐문자열을 return 한다.
+#"   NCT dream buffering      "
+print('e' in s) #True  특정 문자열안에 e가 있나없나 확인
+print('z' in s) #False
+
+#split, join
+ip = '10.253.123.119'
+ip_List = ip.split('.')
+print(ip_List)
+names = '양다연, 최자윤, 임채영, 이예진, 인소리'
+name_List = names.split(',')
+print(name_List)
+print(name_List[2])
+print(name_List[2:4])
+ip_list_str = '::'.join(ip_List)
+print(ip_list_str)
+name_List_str = ' | '.join(name_List)
+print(name_List_str)
+print(",".join(name_List))
