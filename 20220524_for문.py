cd169637557944dcbff41d7934360bbd395b234b
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-05-24 13:55:14 +0900

    for문, 중첩 for문, range() 함수

---
+++ 20220524_for문.py
@@ -0,0 +1,108 @@
+# 문자열
+# for ch in "SORI":
+#     print(ch, end='*')
+# print()
+# 리스트
+# for item in ["힙합", "발라드"]:
+#     print(item)
+
+# 튜플
+# for item in (12, 393):
+#     print(item)
+# for a,b in ((12,33),(2,35)):
+#     print(a,b)
+
+# 셋
+# for item in {"WSM", "JAVA", "Python"}:
+#     print(item)
+
+# 딕셔너리
+# pl = {"C":1972, "JAVA":1995, "Python":1991}
+# for item in pl:
+#     print(item)
+# for key in pl.keys():
+#     print(key)
+# for val in pl.values():
+#     print(val)
+# for key, val in pl.items():
+#     print(key, val)
+
+# num_list 문제
+# num_list = [-5, 127, -13, 9, -21, 100]
+# for num in num_list:
+#     if num > 0:
+#         print(num)
+
+# 짝수, 홀수
+# num_list = [13,8,7,55,100,2,12,10,17]
+# for num in num_list:
+#     if num % 2 == 0:
+#         print("{}은 짝수이다".format(num))
+#     else:
+#         print("{}은 홀수이다".format(num))
+# print('------------------------')
+# holzzak = ["짝수", "홀수"]
+# for num in num_list:
+#     print("{}은 {}이다".format(num, holzzak[num%2]))
+# print('------------------------')
+
+# 자리수
+# for num in num_list:
+#     print("{}은 {}자리수이다.".format(num, len(str(num))))
+
+# svt = {"에스쿱스":90, "정한":100, "조슈아":95, "준":50, "호시":85, "디노":25}
+# for name, score in svt.items():
+#     if score >= 60:
+#         print("{}은 합격입니다".format(name))
+#     else:
+#         print("{}은 불합격입니다".format(name))
+
+# range() 함수
+# range(시작값, 종료값, 단계)
+# range(기본값 0, 종료값, 기본값 1)
+# print(list(range(0,10,1)))
+# print(list(range(10,0,-1))) # 단계값이 음수면 역순
+# print(list(range(0,10,5)))
+#
+# print(list(range(0,10)))
+# print(list(range(10)))
+
+# 리스트
+# nctdream = ['런쥔', '제노', '해찬', '마크', '재민', '지성', '천러']
+# for member in nctdream:
+#     print(member)
+# for i in range(0, len(nctdream)):
+#     print(i+1, nctdream[i])
+# print('-------------')
+# for i, member in enumerate(nctdream):
+#     print(i+1, member)
+
+# range함수 문제
+# total = 0
+# for i in range(1,200):
+#     if i% 3 == 0:
+#         total += i
+# print(total)
+
+# sum_val = 0
+# for i in range(500, 1000,5):
+#     sum_val += i
+# print(sum_val)
+
+# print(sum(list(range(500,1000,5))))
+
+# sum()함수, max, min, avg
+# l = [1,2,3,4,5]
+# print(sum(l))
+# print(max(l))
+# print(min(l))
+
+# 2단 구구단 출력
+for i in range(1,10):
+    print("{}x{}={}".format(2,i,i*2))
+
+# 2~9단 구구단
+for i in range(2,10):
+    for j in range(1, 10):
+        print("{}x{}={}".format(i,j,i*j))
+    print()
