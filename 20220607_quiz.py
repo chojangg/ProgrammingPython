043fb7f9756044bdbd7f21f923dc467e4959d470
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-06-07 09:55:34 +0900

    Quiz. 학번 -> 학년, 반, 학과, 번호

--- 20220607_quiz.py
+++ 20220607_quiz.py
@@ -1,10 +1,10 @@
 # 1. 휴대전화번호 입력하면 -, /, 띄어쓰기 없애고 숫자만 출력하자
 phone_number = '010-4658-7240'
-# for i in phone_number:
-#     if i=='-' or i==' 'or i=='/':
-#         continue
-#     print(i, end='')
-# print()
+for i in phone_number:
+    if i=='-' or i==' 'or i=='/':
+        continue
+    print(i, end='')
+print()
 phone_number = phone_number.replace('-', '')
 phone_number = phone_number.replace('/', '')
 phone_number = phone_number.replace(' ', '')
@@ -13,13 +13,19 @@ print(phone_number)
 print('-'*30)
 
 # 2. 학년->학년, 반, 학과, 번호 출력하기
-# student_number = 2108
-# grade = student_number/1000
-# cl = student_number/(1000*grade)
-#
-# if(cl=='1' or cl=='2'):
-
-# print(grade)
+student_number = '2108'
+# if student_number[1] == '1' or student_number[1] == '2':
+#     g = '뉴미디어소프트웨어과'
+# elif student_number[1] == '3' or student_number[1] == '4':
+#     g = '뉴미디어웹솔루션과'
+# elif student_number[1] == '5' or student_number[1] == '6':
+#     g = '뉴미디어디자인과'
+majors = ['','뉴미디어소프트웨어과', '뉴미디어소프트웨어과', '뉴미디어웹솔루션과',
+          '뉴미디어웹솔루션과', '뉴미디어디자인과','뉴미디어디자인과']
+index = int(student_number[1])
+g = majors[index-1]
+print(f'{student_number[0]}학년 {student_number[1]}반')
+print(f'{g} {int(student_number[2:])}번')
 
 #>> 2학년 1반 뉴미디어 소프트웨어과 08번
 #>> 2학년 1반 뉴미디어 소프트웨어과 8번
