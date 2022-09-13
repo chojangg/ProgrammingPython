da47e05cba594da4afa298aba697338d073d7635
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-09-13 10:30:05 +0900

    class Game -> 객체화

---
+++ 20220913_class1.py
@@ -0,0 +1,66 @@
+# 클래스 연습 1
+# 1. 요리, 2. 연예인, 3. 자동차, 4. 게임
+
+'''
+속성
+title: 제목
+genre: 장르
+platform: 플랫폼
+
+행동
+실행하다()
+저장하다()
+로그인하다()
+
+객체
+World Of Warcraft, 마인크래프트, 배틀그라운드
+
+java
+class Game{
+    Stirng title;
+    public String genre;
+    int playform;   // 0: Window 1: macOS 2: iOS 3: Android
+    public Game(title) {    // 특수메소드 : 생성자
+        this.title = title;
+        this.genre = "MMORPG";
+        this.platform = 0;
+    }
+    public void run() {
+    }
+    public boolean login(String id, String password) {
+    }
+    public void save() {
+    }
+    public toString() {
+        return title + genre;
+    }
+}
+예서게임 = new Game("World of Warcraft");
+채영게임 = new Game("배틀 그라운드");
+채영게임.genre = "FPS";
+
+'''
+class Game:
+    def __init__(self, title): # 특수메소드: 생성자
+        self.title = title
+        self.genre = 'MMORPG'
+        self.platform = 0
+
+    def run(self):  # self 중요!
+        print('실행')
+
+    def login(self, id, password):
+        print(f'{id}님 환영합니다')
+
+    def save(self):
+        print('저장완료')
+
+    def __str__(self):  # 특수메소드: 문자열표현
+        return f'{self.title} {self.genre}'
+
+# class -> object: 객체화
+예서게임 = Game('World of Warcraft')
+채영게임 = Game('마인크래프트')
+채영게임.genre = '샌드박스'
+print(예서게임)
+print(채영게임)
