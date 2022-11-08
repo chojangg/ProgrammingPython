294988e95ba6b06f659dd986ff878dc48e1453a6
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-11-08 09:35:47 +0900

    자기 파일 Run 할 때만 실행하자

--- amasvin/drink.py
+++ amasvin/drink.py
@@ -17,17 +17,20 @@ class Drink:
     _CUP_SIZES = ('레귤러', '점보')  #0: 레귤러, 1: 점보
     _SUGARS = ('30%', '50%', '70%', '100%') #0: 30%, 1: 50%, ...
     _ICES = ('없음', '적게', '기본', '많게')
+
     def __init__(self, name, price):
         self.name = name
         self.price = price
         self.cup_size = 0   #0: 레귤러, 1: 점보
         self.sugar = 1  #0: 30%, 1: 50%, 2: 70%, 3: 100%
         self.ice = 2    #0: 없음, 1: 적게, 2: 기본, 3: 많게
+
     def __str__(self):
         return f'이름: {self.name}\t가격: {self.price}원' \
                f'\t컵사이즈: {Drink._CUP_SIZES[self.cup_size]}' \
                f'\t당도: {Drink._SUGARS[self.sugar]}' \
                f'\t얼음량: {Drink._ICES[self.ice]}'
+
     def set_cup_size(self):
         #사용자에게 숫자를 묻자 1: 레귤러, 2: 점보
         for index, cup_size_label in enumerate(Drink._CUP_SIZES):
@@ -40,6 +43,7 @@ class Drink:
         #self.cup_size가 점보(1)일 때, 가격 +500원
         if self.cup_size == 1:
             self.price += 500
+
     def set_sugar(self):
         #옵션 보여주자 1. 30%, 2. 50%, ...
         for index, sugar_label in enumerate(Drink._SUGARS):
@@ -66,6 +70,7 @@ class Drink:
         self.set_sugar()
         self.set_ice()
 
-음료1 = Drink('아메리카노', 1800)
-음료1.order()
-print(음료1)
+if __name__ == '__main__':  # 중요!!!
+    음료1 = Drink('아메리카노', 1800)
+    음료1.order()
+    print(음료1)
