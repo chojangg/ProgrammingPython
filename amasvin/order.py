9a77ab846f87289f4625a7df037d8bc90e794aeb
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-11-22 09:10:37 +0900

    order 주문내역 출력, 가격 총합계 출력, amasvin에서 실행

--- amasvin/order.py
+++ amasvin/order.py
@@ -1,48 +1,55 @@
 from copy import copy
 
-from coffee import Coffee   #coffee: 모듈(.py), Coffee: 클래스
+from coffee import Coffee   # coffee: 모듈(.py), Coffee: 클래스
 from bubbletea import Bubbletea
 
 
 class Order:
-    def __init__(self):
-        self.menu = []  #메뉴판
-        self.init_menu()    #메뉴판 초기화
+    def __init__(self): # 생성자
+        self.menu = []  # 메뉴판
+        self.init_menu()    # 메뉴판 초기화
 
-        self.order_menu = []     #주문한 음료수 리스트
+        self.order_menu = []     # 주문한 음료수 리스트
 
     def __str__(self):
         # self.order_menu에서 drink 하나씩 꺼내서 출력하자
         # 총 주문금액 출력하자
+        total_price = 0
+        for drink in self.order_menu:
+            total_price += drink.price
 
-        return '\n'.join(str(drink) for drink in self.order_menu)
+        return '\n'.join(str(drink) for drink in self.order_menu)\
+               +f'\n총 주문금액은 {total_price}원입니다.'
 
-    def init_menu(self):    #메뉴판 초기화
+    def init_menu(self):    # 메뉴판 초기화
         new_menu = Bubbletea("하동녹차오레오", 4500)
-        self.menu.append(new_menu)
+        self.menu.append(new_menu)  # append 함수(리스트에 추가)
         new_menu = Coffee("카페 모카", 3000)
         self.menu.append(new_menu)
         new_menu = Bubbletea("라즈베리소다", 2900)
         self.menu.append(new_menu)
 
     def order(self):
-        #반복
+        # 반복
         while True:
-            self.show_menu()    #메뉴판 보여주자
+            self.show_menu()    # 메뉴판 보여주자
             #음료수 고르자(사용자 입력받자)
             choice = input("원하는 음료수를 고르세요(엔터치면 끝): ")
             # 엔터치면 끝
             if choice == '':
                 break
-            #새로운 음료수로 생성하고, 옵션들 주문받자
+            # 새로운 음료수로 생성하고, 옵션들 주문받자
             new_drink = copy(self.menu[int(choice) - 1])
                 # 메뉴 그대로 가져오면, 옵션에 따라 메뉴가 바뀌어있음
                 # 메뉴는 기본으로 놔두고, 복사해와서 그것의 옵션을 바꾸자
-            new_drink.order()
+            new_drink.order()   # 음료 주문받자(옵션)
             # print(new_drink)
-            #주문한 음료수 리스트에 새로운 음료수 추가하자
+            # 주문한 음료수 리스트에 새로운 음료수 추가하자
             self.order_menu.append(new_drink)
-        #주문한 음료수 리스트 출력하자
+        # 주문한 음료수 리스트 출력하자
+        print("-" * 50)
+        print("주문하신 음료수는 다음과 같습니다.")
+        print(self) # __str__ 함수 호출
 
     def show_menu(self):
         for index, drink in enumerate(self.menu):
@@ -51,4 +58,3 @@ class Order:
 if __name__ == '__main__':
     order = Order()
     order.order()
-    print(order)
